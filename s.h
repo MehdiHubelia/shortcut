c7add8f937ccffe345e4d48440fc26f0ec490d88
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-31 15:51:43 -0400

    remove cout

--- s.h
+++ s.h
@@ -49,13 +49,11 @@ private:
 
     static string ltrim(string& s)
     {
-        std::cout << "ltrim: " << &s << std::endl;
         size_t start = s.find_first_not_of(WHITESPACE);
         return (start == string::npos) ? "" : s.substr(start);
     }
     static string rtrim(string s)
     {
-        std::cout << "rtrim: " << &s << std::endl;
         size_t end = s.find_last_not_of(WHITESPACE);
         return (end == string::npos) ? "" : s.substr(0, end + 1);
     }
